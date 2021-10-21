123dd5d1d22634c90eaed033ad7f5e632be68de4
nafik@email.com <nafik@email.com>
2021-10-21 11:31:45 +0700

    tambah db

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -8,70 +8,38 @@ use App\Models\Siswa;
 
 class SiswaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         $siswa_list = Siswa::all();
         return view('siswa.index', ['siswa_list' => $siswa_list]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
         return view('siswa.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
         Siswa::create($request->all());
         return redirect('siswa');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $siswa = Siswa::findOrFail($id);
         return view('siswa.show', ['siswa' => $siswa]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $siswa = Siswa::findOrFail($id);
         return view('siswa.edit', ['siswa' => $siswa]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $siswa = Siswa::findOrFail($id);
