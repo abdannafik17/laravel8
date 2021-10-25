dee96d4bd857c9a3cb295cf09a16ee095b6c59e3
nafik@email.com <nafik@email.com>
2021-10-25 11:52:27 +0700

    hari ke 5

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,15 @@ Route::get('master/halaman-pegawai', function(){
 Route::get('emp', function(){
     return redirect()->route('employee');
 });
+
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
